6fafe48fb011264603f088e233ccc767ba18818a
bzzzbzzzbzz <s.meln1k@ukr.net>
2024-03-14 21:14:24 +0100

    delete useless code

--- online_library.py
+++ online_library.py
@@ -1,21 +1,26 @@
+import time
+
 import requests
 import os
 from pathvalidate import sanitize_filename
-from parsing import get_text_name, get_image_link, parse_book_page
+from parsing import parse_book_page
 from urllib.parse import urlsplit
 import argparse
 
 
-def check_for_redirect(response):
+def check_for_redirect(url):
+    response = requests.get(url)
+    response.raise_for_status()
     if response.history:
         raise requests.HTTPError('Redirect detected')
 
 
-def download_txt(url, filename, folder='books/'):
+def download_txt(book_num, filename, folder='books/'):
     os.makedirs('books', exist_ok=True)
-    response = requests.get(url)
+    url = 'https://tululu.org/txt.php'
+    payload = {'id': book_num}
+    response = requests.get(url, params=payload)
     response.raise_for_status()
-    check_for_redirect(response)
     valid_filename = f'{sanitize_filename(filename)}.txt'
     valid_path = os.path.join(folder, valid_filename)
     with open(valid_path, 'wb') as file:
@@ -36,18 +41,33 @@ def download_image(url, folder='images/'):
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='enter books number')
+    parser = argparse.ArgumentParser(description='Choose diapason of books. For example if you chose start_id 10, '
+                                                 'and end_id 20 '
+                                                 '```python3 online_library.py --start_id 10 --end_id 20``` '
+                                                 'it will download books from number 10 to 20 and their '
+                                                 'images')
     parser.add_argument('--start_id', help='enter first book: ', default=1)
-    parser.add_argument('--end_id', help='enter last book ', default=11)
+    parser.add_argument('--end_id', help='enter last book ', default=10)
     args = parser.parse_args()
-    for i in range(int(args.start_id), int(args.end_id)):
-        text_url = f"https://tululu.org/txt.php?id={i}"
-        book_url = f'https://tululu.org/b{i}/'
-        filename = get_text_name(book_url)
+    fail_connection = 0
+    for book_number in range(int(args.start_id), int(args.end_id)+1):
+        book_url = f'https://tululu.org/b{book_number}/'
         try:
-            download_txt(text_url, filename)
-            image_link = get_image_link(book_url)
+            check_for_redirect(book_url)
+            book_parser = parse_book_page(book_url)
+            filename = book_parser['Название']
+            download_txt(book_number, filename)
+            image_link = book_parser['Картинка']
             download_image(image_link)
-            print(parse_book_page(book_url))
+            print(book_parser)
         except requests.HTTPError:
+            print(f'HTTP Error. Probably, {book_url} is not book.')
             continue
+        except requests.ConnectionError:
+            print('No connection')
+            fail_connection += 1
+            if fail_connection > 1:
+                time.sleep(60)
+            print('Trying to connect...')
+            continue
+
--- parsing.py
+++ parsing.py
@@ -3,25 +3,6 @@ from bs4 import BeautifulSoup
 from urllib.parse import urljoin
 
 
-def get_text_name(url):
-    response = requests.get(url)
-    response.raise_for_status()
-
-    soup = BeautifulSoup(response.text, 'lxml')
-    text_name = soup.find('table').find('h1').text.split('::')[0]
-    return text_name
-
-
-def get_image_link(url):
-    response = requests.get(url)
-    response.raise_for_status()
-
-    soup = BeautifulSoup(response.text, 'lxml')
-    image_link = soup.find(class_='bookimage').find('img')['src']
-    valid_link = urljoin('https://tululu.org/', image_link)
-    return valid_link
-
-
 def parse_book_page(url):
     response = requests.get(url)
     response.raise_for_status()
@@ -29,13 +10,13 @@ def parse_book_page(url):
     book_name = soup.find('table').find('h1').text.split('::')[0].strip()
     author = soup.find('table').find('h1').text.split('::')[1].strip()
     parse_comments = soup.find('table').find_all('div', class_='texts')
-    comments = []
-    for comment in parse_comments:
-        comments.append(comment.text.split(')')[1])
-    genre = soup.find_all('span', class_='d_book')[0].text.split(':')
-    book_information = {'Автор: ': author,
-                        'Название: ': book_name,
-                        'Жанр: ': genre[1].strip(),
-                        'Комментарии: ': comments}
+    comments = [comment.text.split(')')[1] for comment in parse_comments]
+    genre = soup.find_all('span', class_='d_book')[0].text.split(':')[1].strip()
+    image_link = urljoin(url, soup.find(class_='bookimage').find('img')['src'])
+    book = {'Автор': author,
+            'Название': book_name,
+            'Жанр': genre,
+            'Комментарии': comments,
+            'Картинка': image_link}
 
-    return book_information
+    return book
