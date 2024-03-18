187ee14ab67e7d1288ce52ea64f47e37a5b2c4a9
bzzzbzzzbzz <s.meln1k@ukr.net>
2024-03-18 20:20:57 +0100

    deleted verd in value

--- parsing.py
+++ parsing.py
@@ -7,8 +7,8 @@ def parse_book_page(response):
     soup = BeautifulSoup(response.text, 'lxml')
     book_name = soup.find('table').find('h1').text.split('::')[0].strip()
     author = soup.find('table').find('h1').text.split('::')[1].strip()
-    parse_comments = soup.find('table').find_all('div', class_='texts')
-    comments = [comment.text.split(')')[1] for comment in parse_comments]
+    all_comments = soup.find('table').find_all('div', class_='texts')
+    comments = [comment.text.split(')')[1] for comment in all_comments]
     genre = soup.find_all('span', class_='d_book')[0].text.split(':')[1].strip()
     image_link = urljoin(response.url, soup.find(class_='bookimage').find('img')['src'])
     book = {'Автор': author,
